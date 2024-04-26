020d6b1a51cb8d7bc74c4cbb07d04f8cf2cb2751
Manaila Robert <robert122003@gmail.com>
2024-04-26 11:40:15 +0300

    Finished shop window

--- src/main/java/org/example/ShopWindow.java
+++ src/main/java/org/example/ShopWindow.java
@@ -13,6 +13,7 @@ public class ShopWindow extends JFrame {
     JPanel planesPanel = new JPanel();
     JPanel listPlanesPanel = new JPanel();
     JLabel Label_Planes = new JLabel("Planes");
+    JPanel planesLabelPanel = new JPanel();
     JList LB_Planes = new JList<>(new String[]{" Item 1"," Item 2"," Item 3"," Item 4"});
     JPanel BuyPlanesPanel = new JPanel();
     JTextField TB_PlaneQTY = new JTextField();
@@ -22,6 +23,7 @@ public class ShopWindow extends JFrame {
     JPanel crewPanel = new JPanel();
     JPanel listCrewPanel = new JPanel();
     JLabel Label_Crew = new JLabel("Crew");
+    JPanel crewLabelPanel = new JPanel();
     JList LB_Crew =new JList(new String[]{" Item 1"," Item 2"," Item 3"," Item 4"});
     JPanel BuyCrewPanel = new JPanel();
     JTextField TB_CrewQTY = new JTextField();
@@ -56,10 +58,29 @@ public class ShopWindow extends JFrame {
         Font f = new Font("Times New Roman",Font.BOLD,14);
 
 
-        planesPanel.setPreferredSize(new Dimension(290,450));
-        detailsPanel.setPreferredSize(new Dimension(290,450));
-        crewPanel.setPreferredSize(new Dimension(290,450));
 
+
+
+
+        planesPanel.setPreferredSize(new Dimension(290,450));
+        listPlanesPanel.setPreferredSize(new Dimension(290,400));
+        LB_Planes.setPreferredSize(new Dimension(290,400));
+        planesLabelPanel.setAlignmentX(Component.RIGHT_ALIGNMENT);
+
+        detailsPanel.setPreferredSize(new Dimension(290,450));
+        listDetailsPanel.setPreferredSize(new Dimension(290,450));
+        LB_Details.setPreferredSize(new Dimension(290,400));
+        Label_Details.setVerticalAlignment(SwingConstants.BOTTOM);
+
+        crewPanel.setPreferredSize(new Dimension(290,450));
+        listCrewPanel.setPreferredSize(new Dimension(290,450));
+        LB_Crew.setPreferredSize(new Dimension(290,400));
+        crewLabelPanel.setAlignmentX(Component.RIGHT_ALIGNMENT);
+
+
+        listPlanesPanel.setBackground(Color.pink);
+        listDetailsPanel.setBackground(Color.pink);
+        listCrewPanel.setBackground(Color.pink);
     }
 
     private void addUI(){
@@ -69,10 +90,12 @@ public class ShopWindow extends JFrame {
 
         //planes panel
         planesPanel.setLayout(new BoxLayout(planesPanel,BoxLayout.Y_AXIS));
-        planesPanel.add(Label_Planes);
+        planesLabelPanel.add(Label_Planes);
+        planesPanel.add(planesLabelPanel);
         listPlanesPanel.add(LB_Planes);
         planesPanel.add(listPlanesPanel);
-        BuyPlanesPanel.setLayout(new GridLayout(1,2));
+        BuyPlanesPanel.setLayout(new GridLayout(1,3));
+        BuyPlanesPanel.add(new JLabel("Buy QTY:"));
         BuyPlanesPanel.add(TB_PlaneQTY);
         BuyPlanesPanel.add(BTN_BuyPlanes);
         planesPanel.add(BuyPlanesPanel);
@@ -88,10 +111,12 @@ public class ShopWindow extends JFrame {
 
         //crew panel
         crewPanel.setLayout(new BoxLayout(crewPanel,BoxLayout.Y_AXIS));
-        crewPanel.add(Label_Crew);
+        crewLabelPanel.add(Label_Crew);
+        crewPanel.add(crewLabelPanel);
         listCrewPanel.add(LB_Crew);
         crewPanel.add(listCrewPanel);
-        BuyCrewPanel.setLayout(new GridLayout(1,2));
+        BuyCrewPanel.setLayout(new GridLayout(1,3));
+        BuyCrewPanel.add(new JLabel("Buy QTY:"));
         BuyCrewPanel.add(TB_CrewQTY);
         BuyCrewPanel.add(BTN_BuyCrew);
         crewPanel.add(BuyCrewPanel);
