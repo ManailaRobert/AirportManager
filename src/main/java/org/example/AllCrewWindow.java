3d72575af299c557efbae7a561f9114831a206f8
Manaila Robert <robert122003@gmail.com>
2024-04-29 13:15:42 +0300

    Finished AllCrewWindow

--- src/main/java/org/example/AllCrewWindow.java
+++ src/main/java/org/example/AllCrewWindow.java
@@ -5,13 +5,15 @@ import java.awt.*;
 
 public class AllCrewWindow extends JFrame {
 
+    JPanel Top = new JPanel();
     JPanel AvailableCrewPanel = new JPanel();
     JLabel Label_AvailableCrew = new JLabel("Available crew:");
-    JTable AvailableCrewTabel = new JTable();
+    JTable AvailableCrewTabel = new JTable(10,5);
 
+    JPanel Bottom = new JPanel();
     JPanel DispatchedCrewPanel = new JPanel();
     JLabel Label_DispatchedCrew = new JLabel("Dispatched crew:");
-    JTable DispatchedCrewTable = new JTable();
+    JTable DispatchedCrewTable = new JTable(10,5);
 
 
     public  AllCrewWindow(){
@@ -35,14 +37,37 @@ public class AllCrewWindow extends JFrame {
         Label_AvailableCrew.setFont(f);
         AvailableCrewTabel.setFont(f);
         AvailableCrewTabel.setBackground(Color.LIGHT_GRAY);
+        AvailableCrewTabel.setEnabled(false);
+        AvailableCrewTabel.setAutoResizeMode(5);
+        AvailableCrewTabel.getTableHeader().setResizingAllowed(false);
+        AvailableCrewTabel.getTableHeader().setReorderingAllowed(false);
 
         Label_DispatchedCrew.setFont(f);
         DispatchedCrewTable.setFont(f);
         DispatchedCrewTable.setBackground(Color.LIGHT_GRAY);
+        DispatchedCrewTable.setEnabled(false);
+        DispatchedCrewTable.setAutoResizeMode(5);
+        DispatchedCrewTable.getTableHeader().setResizingAllowed(false);
+        DispatchedCrewTable.getTableHeader().setReorderingAllowed(false);
     }
 
     private void addUI() {
 
 
+        Top.setLayout(new BoxLayout(Top,BoxLayout.Y_AXIS));
+
+        Top.add(Label_AvailableCrew);
+        AvailableCrewPanel.add(new JScrollPane(AvailableCrewTabel));
+        Top.add(AvailableCrewPanel);
+
+
+        Bottom.setLayout(new BoxLayout(Bottom,BoxLayout.Y_AXIS));
+
+        Bottom.add(Label_DispatchedCrew);
+        DispatchedCrewPanel.add(new JScrollPane(DispatchedCrewTable));
+        Bottom.add(DispatchedCrewPanel);
+
+        add(Top);
+        add(Bottom);
     }
 }
